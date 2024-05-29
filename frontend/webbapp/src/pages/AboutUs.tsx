f1fb59eb083ac8a82fa8d7acdcd4a0248b9b4482
Elina Kerola <elinak90@icloud.com>
2024-05-29 23:45:44 +0200

    about us start

--- frontend/webbapp/src/pages/AboutUs.tsx
+++ frontend/webbapp/src/pages/AboutUs.tsx
@@ -11,8 +11,12 @@ const AboutUs: FC = () => {
           flexDirection: "column",
           alignItems: "center",
           justifyContent: "center",
-          width: "100%", 
-         
+          width: "100%",
+          border: "1px solid #d29bbf",
+          borderRadius: "10px",
+          boxShadow: "0 4px 12px rgba(0, 0, 0, 0.1)",
+          backgroundColor: "#fff",
+          
         }}
       >
         <Box
@@ -25,7 +29,8 @@ const AboutUs: FC = () => {
             objectFit: "cover",
             borderRadius: "10px",
             boxShadow: "0 4px 12px rgba(0, 0, 0, 0.1)",
-            marginBottom: "40px",
+            marginBottom: "20px",
+            filter: "grayscale(20%)",
           }}
         />
         <Typography variant="h2" gutterBottom sx={{ textAlign: "center", color: "#d29bbf", position: "relative" }}>
@@ -39,21 +44,25 @@ const AboutUs: FC = () => {
               width: "80px",
               height: "3px",
               backgroundColor: "#d29bbf",
+              borderRadius: "5px",
+              opacity: 0.7,
             }}
           />
         </Typography>
         <Box
           sx={{
             backgroundColor: "#f4f4f4",
-            padding: 3,
+            padding: "20px",
             maxWidth: "800px",
             margin: "0 auto",
             textAlign: "left",
-            borderRadius: 1,
+            borderRadius: "10px",
             boxShadow: "0 2px 10px rgba(0,0,0,0.1)",
+            marginBottom: "20px",
+            filter: "contrast(120%)",
           }}
         >
-          <Typography variant="body1" paragraph>
+          <Typography variant="body1" paragraph sx={{ color: "#555" }}>
             <strong>Med känsla för ett dammfritt resultat</strong>
           </Typography>
           <Typography variant="body1" paragraph>
@@ -74,18 +83,31 @@ const AboutUs: FC = () => {
             dammsugaren efter sig. Det går undan men rent och prydligt blir det.
           </Typography>
         </Box>
-        <Typography variant="body1" paragraph sx={{ marginTop: "40px" }}>
-          Vi skräddarsyr ett upplägg som matchar ditt unika boende eller företag.
-          Kanske önskas lakansbyte i hemmet eller hjälp med återvinningen på
-          företaget? Berätta vad du önskar få hjälp med så sammanställer vi allt
-          tillsammans i en städbeskrivning där det också framgår vilka rum som ska
-          ingå och hur du vill ha städningen utförd.
-        </Typography>
-        <Typography variant="body1" paragraph>
-          Det ger ett resultat med god kvalitet och kontinuitet. Allt för att du
-          ska kunna släppa tankarna på städningen och fokusera på annat under
-          tiden.
-        </Typography>
+        <Box
+          sx={{
+            backgroundColor: "#f4f4f4",
+            padding: "20px",
+            maxWidth: "800px",
+            margin: "0 auto",
+            textAlign: "left",
+            borderRadius: "10px",
+            boxShadow: "0 2px 10px rgba(0,0,0,0.1)",
+            filter: "contrast(120%)",
+          }}
+        >
+          <Typography variant="body1" paragraph>
+            Vi skräddarsyr ett upplägg som matchar ditt unika boende eller företag.
+            Kanske önskas lakansbyte i hemmet eller hjälp med återvinningen på
+            företaget? Berätta vad du önskar få hjälp med så sammanställer vi allt
+            tillsammans i en städbeskrivning där det också framgår vilka rum som ska
+            ingå och hur du vill ha städningen utförd.
+          </Typography>
+          <Typography variant="body1" paragraph>
+            Det ger ett resultat med god kvalitet och kontinuitet. Allt för att du
+            ska kunna släppa tankarna på städningen och fokusera på annat under
+            tiden.
+          </Typography>
+        </Box>
       </Box>
     </Fade>
   );
