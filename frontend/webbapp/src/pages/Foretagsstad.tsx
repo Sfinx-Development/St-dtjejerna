5787029827a95127164d03199d9362b64091237e
Elina Kerola <elinak90@icloud.com>
2025-03-27 12:25:54 +0100

    design och seo städtjejerna

--- frontend/webbapp/src/pages/Foretagsstad.tsx
+++ frontend/webbapp/src/pages/Foretagsstad.tsx
@@ -1,3 +1,196 @@
+// import BathtubIcon from "@mui/icons-material/Bathtub";
+// import KitchenIcon from "@mui/icons-material/Kitchen";
+// import MeetingRoomIcon from "@mui/icons-material/MeetingRoom";
+// import { Box, Typography } from "@mui/material";
+// import { FC } from "react";
+// import { useNavigate } from "react-router-dom";
+// import CustomButton from "../components/CustomButton";
+// import Info from "../components/Info";
+// import { useScreenSize } from "../screenSizeContext";
+// import HelmetWrapper from "../components/HelmetWrapper";
+
+// const Foretagsstad: FC = () => {
+//   const { isMobile } = useScreenSize();
+//   const navigate = useNavigate();
+
+//   const sections = [
+//     {
+//       title: "Kontor",
+//       icon: (
+//         <MeetingRoomIcon
+//           sx={{ fontSize: 30, color: "#e48bb1", marginRight: 1 }}
+//         />
+//       ),
+//       items: [
+//         "Dammsugning av alla golv och mattor samt våttorkning av golven. Dammtorkning av alla fria ytor. Fläckar på speglar, lister och dörrar torkas bort. Papperskorgarna töms.",
+//       ],
+//     },
+//     {
+//       title: "Toaletter",
+//       icon: (
+//         <BathtubIcon sx={{ fontSize: 30, color: "#e48bb1", marginRight: 1 }} />
+//       ),
+//       items: [
+//         "Dammsugning och våttorkning av golven. Handfat och toaletter skuras både in- och utvändigt.",
+//       ],
+//     },
+//     {
+//       title: "Kök/Pentry",
+//       icon: (
+//         <KitchenIcon sx={{ fontSize: 30, color: "#e48bb1", marginRight: 1 }} />
+//       ),
+//       items: [
+//         "Dammsugning och våttorkning av golven. Arbetsbänk och diskbänk rengörs grundligt.",
+//       ],
+//     },
+//   ];
+
+//   return (
+//     <>
+//       <HelmetWrapper
+//         title="Företagsstäd i Borås | Kontorsstäd med Kvalitet – Städtjejerna"
+//         description="Städtjejerna erbjuder professionell företagsstädning i Borås för kontor, butiker och verksamheter. Flexibla upplägg med fasta priser."
+//         url="https://www.stadtjejerna.se/foretagsstad"
+//         image="https://i.imgur.com/aCLoUIE.jpeg"
+//       />
+
+//       <Box
+//         sx={{
+//           backgroundImage: 'url("https://i.imgur.com/aCLoUIE.jpeg")',
+//           minHeight: "800px",
+//           backgroundAttachment: "fixed",
+//           backgroundPosition: "center",
+//           backgroundRepeat: "no-repeat",
+//           backgroundSize: "cover",
+//           display: "flex",
+//           alignItems: "center",
+//           width: "100%",
+//           justifyContent: "center",
+//           color: "white",
+//           fontSize: "1.5rem",
+//           textAlign: "center",
+//           padding: "4rem 2rem",
+//         }}
+//       >
+//         <Typography
+//           variant={isMobile ? "h4" : "h2"}
+//           sx={{
+//             backgroundColor: "rgba(0, 0, 0, 0.6)",
+//             padding: "20px",
+//             borderRadius: "10px",
+//             zIndex: 1,
+//             maxWidth: "80%",
+//             textAlign: "center",
+//             marginBottom: 20,
+//           }}
+//         >
+//           Företagsstäd
+//         </Typography>
+//       </Box>
+
+//       <Box
+//         sx={{
+//           maxWidth: isMobile ? "100%" : "900px",
+//           margin: "0 auto",
+//           padding: { xs: 4, md: 8 },
+//           backgroundColor: "#fff",
+//           borderRadius: "8px",
+//           // boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+//           marginBottom: "2rem",
+//         }}
+//       >
+//         <Typography variant="h6" paragraph sx={{ color: "#555" }}>
+//           <strong>
+//             Städning på företag och kontor som skapar en god arbetsmiljö för dig
+//             och dina medarbetare.
+//           </strong>
+//         </Typography>
+//         <Typography
+//           variant="body1"
+//           sx={{ lineHeight: 1.8, color: "#555", marginTop: 2 }}
+//         >
+//           Vi tillbringar många timmar i veckan på våra arbetsplatser och det är
+//           viktigt att trivas och ha möjlighet att fokusera på sina
+//           arbetsuppgifter. Städtjejerna riktar sig till en bred målgrupp och är
+//           redo att erbjuda sina professionella städtjänster till företag med
+//           stora kontorslandskap, industrilokaler eller mindre butiker och kontor
+//           runtom i Borås. Oavsett vilket som passar in på ditt företag är den
+//           gemensamma nämnaren att städningen skapar en god arbetsmiljö för dig
+//           och dina medarbetare samt för de som besöker företaget.
+//         </Typography>
+//         <Typography
+//           variant="body1"
+//           sx={{ lineHeight: 1.8, color: "#555", marginTop: 2 }}
+//         >
+//           Ett rent och fräscht intryck gör att alla kan fokusera på sitt jobb
+//           och slippa tänka på om papperskorgen blir tömd eller om dammråttorna i
+//           hörnen kommer städas bort. Det stärker också företagets varumärke och
+//           gör personalen friskare. Välkommen att kontakta Städtjejerna för att
+//           gå igenom vårt standardutbud samt göra anpassade tillägg som ditt
+//           företag önskar. Vi är flexibla och är lyhörda när det gäller varje
+//           kunds speciella önskemål.
+//         </Typography>
+
+//         <Typography
+//           variant="body1"
+//           sx={{ lineHeight: 1.8, color: "#555", marginTop: 2 }}
+//         >
+//           Städtjejerna tar med sig de rengöringsmedlen som behövs och alla medel
+//           ingår i priset. Det finns möjlighet att beställa förbrukningsmaterial
+//           som exempelvis tvål, toalettpapper, och pappershanddukar via oss.
+//         </Typography>
+//         <CustomButton
+//           title="Få ett kostnadsförslag"
+//           ariaLabel="Till offertsidan"
+//           disabled={false}
+//           animation={false}
+//           handleOnClik={() => navigate("/offert")}
+//         />
+//       </Box>
+
+//       <>
+//         {sections.map((section, index) => (
+//           <Box
+//             key={index}
+//             sx={{
+//               maxWidth: isMobile ? "100%" : "900px",
+//               margin: "0 auto",
+//               padding: { xs: 4, md: 8 },
+//               borderRadius: "8px",
+//               // boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
+//               marginLeft: isMobile ? 0 : 15,
+//               marginBottom: "2rem",
+//             }}
+//           >
+//             <Typography
+//               variant="h5"
+//               sx={{ marginBottom: "0.5rem", color: "#555" }}
+//             >
+//               <strong>{section.title}</strong>
+//             </Typography>
+//             {section.items.map((item, idx) => (
+//               <Box
+//                 key={idx}
+//                 sx={{
+//                   display: "flex",
+//                   alignItems: "center",
+//                   marginBottom: "0.3rem",
+//                 }}
+//               >
+//                 <Box sx={{ marginRight: "0.5rem" }}>{section.icon}</Box>
+//                 <Typography variant="body1">{item}</Typography>
+//               </Box>
+//             ))}
+//           </Box>
+//         ))}
+//       </>
+
+//       <Info />
+//     </>
+//   );
+// };
+
+// export default Foretagsstad;
 import BathtubIcon from "@mui/icons-material/Bathtub";
 import KitchenIcon from "@mui/icons-material/Kitchen";
 import MeetingRoomIcon from "@mui/icons-material/MeetingRoom";
@@ -15,32 +208,28 @@ const Foretagsstad: FC = () => {
 
   const sections = [
     {
-      title: "Kontor",
-      icon: (
-        <MeetingRoomIcon
-          sx={{ fontSize: 30, color: "#e48bb1", marginRight: 1 }}
-        />
-      ),
+      title: "Kontorsstädning",
+      icon: <MeetingRoomIcon sx={{ fontSize: 30, color: "#e48bb1", marginRight: 1 }} />,
       items: [
-        "Dammsugning av alla golv och mattor samt våttorkning av golven. Dammtorkning av alla fria ytor. Fläckar på speglar, lister och dörrar torkas bort. Papperskorgarna töms.",
+        "Dammsugning och våttorkning av golv och mattor.",
+        "Dammtorkning av fria ytor, speglar, lister och dörrar.",
+        "Tömning av papperskorgar och avfläckning av ytor.",
       ],
     },
     {
       title: "Toaletter",
-      icon: (
-        <BathtubIcon sx={{ fontSize: 30, color: "#e48bb1", marginRight: 1 }} />
-      ),
+      icon: <BathtubIcon sx={{ fontSize: 30, color: "#e48bb1", marginRight: 1 }} />,
       items: [
-        "Dammsugning och våttorkning av golven. Handfat och toaletter skuras både in- och utvändigt.",
+        "Skurning av toaletter och handfat, invändigt och utvändigt.",
+        "Rengöring av speglar, golv, väggar och andra ytor.",
       ],
     },
     {
-      title: "Kök/Pentry",
-      icon: (
-        <KitchenIcon sx={{ fontSize: 30, color: "#e48bb1", marginRight: 1 }} />
-      ),
+      title: "Kök & Pentry",
+      icon: <KitchenIcon sx={{ fontSize: 30, color: "#e48bb1", marginRight: 1 }} />,
       items: [
-        "Dammsugning och våttorkning av golven. Arbetsbänk och diskbänk rengörs grundligt.",
+        "Rengöring av arbetsbänk, diskbänk och ytor.",
+        "Dammsugning och våttorkning av golv.",
       ],
     },
   ];
@@ -48,8 +237,8 @@ const Foretagsstad: FC = () => {
   return (
     <>
       <HelmetWrapper
-        title="Företagsstäd i Borås | Kontorsstäd med Kvalitet – Städtjejerna"
-        description="Städtjejerna erbjuder professionell företagsstädning i Borås för kontor, butiker och verksamheter. Flexibla upplägg med fasta priser."
+        title="Företagsstädning i Borås | Professionell kontorsstäd – Städtjejerna"
+        description="Vi erbjuder noggrann och flexibel företagsstädning i Borås för kontor, butiker och verksamheter. Fast pris, miljövänliga medel och god service."
         url="https://www.stadtjejerna.se/foretagsstad"
         image="https://i.imgur.com/aCLoUIE.jpeg"
       />
@@ -76,69 +265,44 @@ const Foretagsstad: FC = () => {
           variant={isMobile ? "h4" : "h2"}
           sx={{
             backgroundColor: "rgba(0, 0, 0, 0.6)",
-            padding: "20px",
+            padding: "20px 40px",
             borderRadius: "10px",
-            zIndex: 1,
             maxWidth: "80%",
-            textAlign: "center",
-            marginBottom: 20,
           }}
         >
-          Företagsstäd
+          Företagsstädning i Borås
         </Typography>
       </Box>
 
       <Box
         sx={{
-          maxWidth: isMobile ? "100%" : "900px",
+          maxWidth: "900px",
           margin: "0 auto",
           padding: { xs: 4, md: 8 },
           backgroundColor: "#fff",
           borderRadius: "8px",
-          // boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
           marginBottom: "2rem",
+          color: "#555",
         }}
       >
         <Typography variant="h6" paragraph sx={{ color: "#555" }}>
           <strong>
-            Städning på företag och kontor som skapar en god arbetsmiljö för dig
-            och dina medarbetare.
+          Skapa en trivsam arbetsmiljö med professionell städning
           </strong>
         </Typography>
-        <Typography
-          variant="body1"
-          sx={{ lineHeight: 1.8, color: "#555", marginTop: 2 }}
-        >
-          Vi tillbringar många timmar i veckan på våra arbetsplatser och det är
-          viktigt att trivas och ha möjlighet att fokusera på sina
-          arbetsuppgifter. Städtjejerna riktar sig till en bred målgrupp och är
-          redo att erbjuda sina professionella städtjänster till företag med
-          stora kontorslandskap, industrilokaler eller mindre butiker och kontor
-          runtom i Borås. Oavsett vilket som passar in på ditt företag är den
-          gemensamma nämnaren att städningen skapar en god arbetsmiljö för dig
-          och dina medarbetare samt för de som besöker företaget.
-        </Typography>
-        <Typography
-          variant="body1"
-          sx={{ lineHeight: 1.8, color: "#555", marginTop: 2 }}
-        >
-          Ett rent och fräscht intryck gör att alla kan fokusera på sitt jobb
-          och slippa tänka på om papperskorgen blir tömd eller om dammråttorna i
-          hörnen kommer städas bort. Det stärker också företagets varumärke och
-          gör personalen friskare. Välkommen att kontakta Städtjejerna för att
-          gå igenom vårt standardutbud samt göra anpassade tillägg som ditt
-          företag önskar. Vi är flexibla och är lyhörda när det gäller varje
-          kunds speciella önskemål.
+
+        <Typography variant="body1" paragraph>
+          En ren arbetsplats ger både ett bra intryck och en hälsosam miljö. Vi på Städtjejerna erbjuder flexibel och noggrann företagsstädning i Borås – oavsett om det gäller kontorslokaler, butiker eller verksamheter av annan typ.
         </Typography>
 
-        <Typography
-          variant="body1"
-          sx={{ lineHeight: 1.8, color: "#555", marginTop: 2 }}
-        >
-          Städtjejerna tar med sig de rengöringsmedlen som behövs och alla medel
-          ingår i priset. Det finns möjlighet att beställa förbrukningsmaterial
-          som exempelvis tvål, toalettpapper, och pappershanddukar via oss.
+        <Typography variant="body1" paragraph>
+          Vårt team arbetar utifrån beprövade checklistor men anpassar oss alltid efter dina behov. Vi erbjuder fasta priser, använder miljövänliga produkter och ser till att du får samma städare vid varje tillfälle för trygghet och kontinuitet.
         </Typography>
+
+        <Typography variant="body1" paragraph>
+          Vi kan även leverera förbrukningsmaterial såsom pappershanddukar, tvål och toalettpapper vid behov.
+        </Typography>
+
         <CustomButton
           title="Få ett kostnadsförslag"
           ariaLabel="Till offertsidan"
@@ -148,42 +312,35 @@ const Foretagsstad: FC = () => {
         />
       </Box>
 
-      <>
-        {sections.map((section, index) => (
-          <Box
-            key={index}
-            sx={{
-              maxWidth: isMobile ? "100%" : "900px",
-              margin: "0 auto",
-              padding: { xs: 4, md: 8 },
-              borderRadius: "8px",
-              // boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.1)",
-              marginLeft: isMobile ? 0 : 15,
-              marginBottom: "2rem",
-            }}
-          >
-            <Typography
-              variant="h5"
-              sx={{ marginBottom: "0.5rem", color: "#555" }}
+      {sections.map((section, index) => (
+        <Box
+          key={index}
+          sx={{
+            maxWidth: "900px",
+            margin: "0 auto",
+            padding: { xs: 4, md: 4 },
+            borderRadius: "8px",
+            marginLeft: isMobile ? 0 : 15,
+            marginBottom: "1rem",
+            backgroundColor: "#fff",
+          }}
+        >
+          <Typography variant="h5" sx={{ marginBottom: "0.5rem", color: "#555" }}>
+            <strong>{section.title}</strong>
+          </Typography>
+          {section.items.map((item, idx) => (
+            <Box
+              key={idx}
+              sx={{ display: "flex", alignItems: "start", marginBottom: "0.3rem" }}
             >
-              <strong>{section.title}</strong>
-            </Typography>
-            {section.items.map((item, idx) => (
-              <Box
-                key={idx}
-                sx={{
-                  display: "flex",
-                  alignItems: "center",
-                  marginBottom: "0.3rem",
-                }}
-              >
-                <Box sx={{ marginRight: "0.5rem" }}>{section.icon}</Box>
-                <Typography variant="body1">{item}</Typography>
-              </Box>
-            ))}
-          </Box>
-        ))}
-      </>
+              <Box sx={{ marginRight: "0.5rem" }}>{section.icon}</Box>
+              <Typography variant="body1" sx={{ color: "#555" }}>
+                {item}
+              </Typography>
+            </Box>
+          ))}
+        </Box>
+      ))}
 
       <Info />
     </>
--- frontend/webbapp/src/pages/RootLayout.tsx
+++ frontend/webbapp/src/pages/RootLayout.tsx
@@ -1,3 +1,57 @@
+// import { Outlet } from "react-router-dom";
+// import CustomHeader from "../components/CustomHeader";
+// import Footer from "../components/Footer";
+// import MobileHeader from "../components/MobileHeader";
+// import ScrollToTop from "../components/ScrollToTop";
+// import { useScreenSize } from "../screenSizeContext";
+
+// const RootLayout = () => {
+//   const { isMobile } = useScreenSize();
+//   return (
+//     <div
+//     // style={{
+//     //   display: "flex",
+//     //   flexDirection: "column",
+//     //   flex: 1,
+//     //   height: "100vh",
+//     //   alignItems: "center",
+//     //   width: "100%",
+//     //   margin: 0,
+//     //   padding: 0,
+//     // }}
+//     >
+//       {isMobile ? <MobileHeader /> : <CustomHeader />}
+//       <main
+//         style={{
+//           display: "flex",
+//           flexDirection: "column",
+//           flexGrow: 1,
+//           width: "100%",
+//           alignItems: "center",
+//         }}
+//       >
+//         <ScrollToTop />
+//         <Outlet />
+//       </main>
+
+//       <footer
+//         style={{
+//           width: "100%",
+//           display: "flex",
+//           flex: 1,
+//           flexDirection: "column",
+//           padding: 0,
+//           margin: 0,
+//           maxWidth: "none",
+//         }}
+//       >
+//         <Footer />
+//       </footer>
+//     </div>
+//   );
+// };
+
+// export default RootLayout;
 import { Outlet } from "react-router-dom";
 import CustomHeader from "../components/CustomHeader";
 import Footer from "../components/Footer";
@@ -7,21 +61,31 @@ import { useScreenSize } from "../screenSizeContext";
 
 const RootLayout = () => {
   const { isMobile } = useScreenSize();
+
   return (
-    <div
-    // style={{
-    //   display: "flex",
-    //   flexDirection: "column",
-    //   flex: 1,
-    //   height: "100vh",
-    //   alignItems: "center",
-    //   width: "100%",
-    //   margin: 0,
-    //   padding: 0,
-    // }}
-    >
-      {isMobile ? <MobileHeader /> : <CustomHeader />}
+    <div>
+      {/* Hoppa till innehåll-länk för tillgänglighet */}
+      <a
+        href="#main-content"
+        style={{
+          position: "absolute",
+          left: "-10000px",
+          top: "auto",
+          width: "1px",
+          height: "1px",
+          overflow: "hidden",
+        }}
+      >
+        Hoppa till innehåll
+      </a>
+
+      <header aria-label="Webbplatsens huvudmeny">
+        {isMobile ? <MobileHeader /> : <CustomHeader />}
+      </header>
+
       <main
+        id="main-content"
+        role="main"
         style={{
           display: "flex",
           flexDirection: "column",
@@ -35,6 +99,7 @@ const RootLayout = () => {
       </main>
 
       <footer
+        aria-label="Webbplatsens sidfot"
         style={{
           width: "100%",
           display: "flex",
@@ -52,3 +117,4 @@ const RootLayout = () => {
 };
 
 export default RootLayout;
+
